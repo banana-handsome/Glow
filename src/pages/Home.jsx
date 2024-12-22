f19e2b59e401eb38f314e239b345b5a491b2a29b
banana-handsome <anonymn235@gmail.com>
2024-12-22 19:01:11 +0700

    Menambahkan Tailwindcss dan React-Router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// src\pages\Home.jsx
+import React from "react";
+
+function Home()
+{
+    return(
+        <div>r
+            <h1>Home</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/Tools.jsx
@@ -0,0 +1,13 @@
+// src\pages\Tools.jsx
+import React from "react";
+
+function Tools()
+{
+    return(
+        <div>
+            <h1>My Tools</h1>
+        </div>
+    )
+}
+
+export default Tools;l
